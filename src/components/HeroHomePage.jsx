a58b30a79f6fc6eb6ffab653c5e94a8b7a621a7f
Mateo Gurrea <mategurrea@gmail.com>
2025-02-14 17:57:11 -0300

    fix in TypeAnimation title

--- src/components/HeroHomePage.jsx
+++ src/components/HeroHomePage.jsx
@@ -2,25 +2,47 @@ import { useTranslation } from 'react-i18next';
 import image from '../assets/personaje.svg'
 import Container from './Container'
 import { TypeAnimation } from 'react-type-animation'
+import { useEffect, useState } from 'react';
 
 export default function HeroHomePage() {
-  const { t } = useTranslation();
+  const { t,i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language)
+
+  useEffect(() => {
+    setLanguage(i18n.language)
+  }, [i18n.language])
+  
   return (
     <Container>
+      {console.log(i18n.language)}
       <div className="w-full text-center h-dvh grid lg:grid-cols-5 grid-cols-1 place-items-center lg:gap-12 lg:pt-0 pt-40">
         <span className="col-start-1 lg:col-span-3 font-family-sans text-[#b6a45e]">
-          <TypeAnimation
+          {language === 'en' ? 
+            <TypeAnimation
             repeat={Infinity}
             className="font-family-sans lg:text-5xl text-2xl text-[#b6a45e]"
             cursor={true}
             sequence={[
-              4000,
-              `${t('Presentation1')}`,
+              'Hello, I am Mateo Gurrea',// `${t('Presentation1')}`,
               3000,
-              `${t('Presentation2')}`,
+              'Hello, I am Web Developer',// `${t('Presentation2')}`,
               5000,
             ]}
-          />
+            />
+           : ''}
+           {language === 'es' ?
+            <TypeAnimation
+            repeat={Infinity}
+            className="font-family-sans lg:text-5xl text-2xl text-[#b6a45e]"
+            cursor={true}
+            sequence={[
+              'Hola, soy Mateo Gurrea',// `${t('Presentation1')}`,
+              3000,
+              'Hola, soy Desarrollador Web',// `${t('Presentation2')}`,
+              5000,
+            ]}
+            />
+            : ''}
         </span>
         <div className="relative overflow-hidden bg-[#b6a45e] h-64 lg:h-96 w-64 lg:w-96 rounded-full bottom-20 lg:bottom-0 lg:col-start-4 lg:col-span-2 drop-shadow-lg">
           <img
